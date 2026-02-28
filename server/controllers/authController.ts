73d531131fb10a267eed45320f679d874ad3ac2d
JayJay-Piyaphat <piyaphat20104@gmail.com>
2026-02-28 21:52:05 +0700

    update login &&auth api

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,6 +1,5 @@
 import { Request,Response } from "express";
 import { PrismaClient } from "@prisma/client";
-import type { IHospital } from "../../shared/types/schema/hospital";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import { AuthRequest } from "../middleware/authMiddleware";
@@ -14,20 +13,29 @@ export const Login = async(req:Request,res:Response):Promise<void> => {
             res.status(400).json({message:"กรุณากรอกข้อมูลให้ครบ"})
             return
         }
-        const hospital = await prisma.hospital.findUnique({where:{id:username}})
-        if(!hospital){
+        const user = await prisma.user.findUnique({
+            where:{username:username},
+            include:{ 
+                hospital: 
+                {include: 
+                    { province: true }}}}
+        )
+        if(!user){
             res.status(400).json({message:"รหัสผ่านหรือไอดีไม่ถูกต้อง"})
             return
         }
-        const isPasswordValid = await bcrypt.compare(password, hospital.password);
+        const isPasswordValid = await bcrypt.compare(password, user.password);
         if(!isPasswordValid){
             res.status(400).json({message:"รหัสผ่านไม่ถูกต้อง"})
             return
         }
         const payload = {
-            id: hospital.id,
-            name: hospital.name,
-            provinceId: hospital.provinceId
+            userId: user.id,
+            username: user.username,
+            hospitalId: user.hospitalId,
+            hospitalName: user.hospital.name,
+            provinceId: user.hospital.provinceId,
+            provinceName: user.hospital.province.name
         };
         const secretKey = process.env.JWT_SECRET || 'fallback_secret';
         const token = jwt.sign(
@@ -49,7 +57,7 @@ export const Login = async(req:Request,res:Response):Promise<void> => {
 
 export const checkMe = async (req:AuthRequest,res:Response):Promise<void> => {
     try {
-        if (!req.hospital) {
+        if (!req.user) {
             res.status(200).json({ 
                 isAuthenticated: false, 
                 role: 'guest',
@@ -60,7 +68,7 @@ export const checkMe = async (req:AuthRequest,res:Response):Promise<void> => {
         res.status(200).json({
             isAuthenticated: true,
             role: 'hospital',
-            hospital: req.hospital
+            hospital: req.user
         });
     } catch (error) {
         console.log(error)
